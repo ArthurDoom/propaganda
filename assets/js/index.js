deb7d87c535990732cd8f3b8b276c68fcb02ad83
ArthurDoom <walterrosscraig@gmail.com>
2019-10-03 02:42:46 +0100

    added contact area, scroll to top btn, minimized images

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,3 +23,25 @@
     });
   }
 })();
+
+//Get the button:
+var scrollButton = document.getElementById("scrollBtn");
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.classList.add("show");
+  } else {
+    scrollButton.classList.remove("show");
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
